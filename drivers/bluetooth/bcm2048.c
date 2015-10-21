99f7abb79bdddca406666b0e79c49c96e217d2ea
Sebastian Reichel <sre@kernel.org>
2015-10-21 15:53:21 +0200

    bcm2048: enable debug

--- drivers/bluetooth/bcm2048.c
+++ drivers/bluetooth/bcm2048.c
@@ -18,6 +18,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#define DEBUG
+
 #include <linux/clk.h>
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
