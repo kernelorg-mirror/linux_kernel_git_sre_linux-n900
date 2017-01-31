ac9d0c1f0732a544a8cdcda15a08477f2af544ce
Sebastian Reichel <sre@kernel.org>
2017-01-31 15:32:46 +0100

    serdev: fix SERIAL_DEV_BUS enabled check
    It's not enough to check for CONFIG_SERIAL_DEV_BUS, since
    serdev may be compiled as module. Fix this by using IS_ENABLED()
    macro, which also checks if the config option has been selected
    as module.
    
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- include/linux/serdev.h
+++ include/linux/serdev.h
@@ -180,7 +180,7 @@ static inline int serdev_controller_receive_buf(struct serdev_controller *ctrl,
 	return serdev->ops->receive_buf(ctrl->serdev, data, count);
 }
 
-#ifdef CONFIG_SERIAL_DEV_BUS
+#if IS_ENABLED(CONFIG_SERIAL_DEV_BUS)
 
 int serdev_device_open(struct serdev_device *);
 void serdev_device_close(struct serdev_device *);
