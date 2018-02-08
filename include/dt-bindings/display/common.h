5f2d46afc61a1c009e9b76a02842747585508dd2
Sebastian Reichel <sebastian.reichel@collabora.co.uk>
2018-02-08 18:53:01 +0100

    dt-bindings: panel: common: document orientation property
    Introduce new "orientation" property for describing in which
    orientation a panel has been mounted to the device. This can
    be used by the operating system to automatically rotate the
    display correctly.
    
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>

---
+++ include/dt-bindings/display/common.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This header provides common constants for displays
+ */
+
+#ifndef _DT_BINDINGS_DISPLAY_COMMON_H
+#define _DT_BINDINGS_DISPLAY_COMMON_H
+
+#define PANEL_ORIENTATION_NORMAL 0
+#define PANEL_ORIENTATION_BOTTOM_UP 1
+#define PANEL_ORIENTATION_LEFT_UP 2
+#define PANEL_ORIENTATION_RIGHT_UP 3
+
+#endif
